37c05f8da19509906bedb71476e48a7e3be0d1ab
Super Cat <123203733+supercat911@users.noreply.github.com>
2023-06-03 08:41:30 +0300

    Update StateManager.js

--- StateManager.js
+++ StateManager.js
@@ -1,111 +1,5 @@
 import { EventEmitter } from "./EventEmitter";
-
-function compareObjects(a, b) {
-    if (a === b) return true;
-
-    if (typeof a != typeof b) return false;
-
-    if (Array.isArray(a)) {
-        return JSON.stringify(a) === JSON.stringify(b);
-    }
-
-    let a_json = JSON.stringify(a, Object.keys(a).sort());
-    let b_json = JSON.stringify(b, Object.keys(b).sort());
-    return a_json === b_json;
-}
-
-class State {
-
-    #getter = null
-    #value = null
-    #name = ''
-    #stateManager = null
-
-    constructor(name = null, value = null, stateManager = null) {
-        this.#name = name;
-        this.#value = value;
-        this.listeners = [];
-        this.#stateManager = stateManager;
-    }
-
-    subscribe(callback) {
-        this.listeners.push(callback);
-        return callback;
-    }
-
-    unsubscribe(callback) {
-        for (let i = 0; i < this.listeners.length; i++) {
-            if (this.listeners[i] === callback) {
-                this.listeners.splice(i, 1);
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    unsubscribeAll() {
-        this.listeners = [];
-    }
-
-    setGetter(func) {
-        this.#getter = func;
-    }
-
-    clearGetter() {
-        this.#getter = null;
-    }
-
-    getValue() {
-        if (this.#getter) {
-            this.#value = this.#getter();
-        }
-
-        return this.#value;
-    }
-
-    getName() {
-        return this.#name;
-    }
-
-    setValue(newValue, __force_write_flag = false) {
-        let previousValue = this.#value;
-
-        if (compareObjects(previousValue, newValue)) {
-            return false;
-        }
-
-        if (!this.#stateManager) {
-
-            this.#value = newValue;
-
-            for (let i = 0; i < this.listeners.length; i++) {
-                let listener = this.listeners[i];
-                try {
-                    listener(newValue, previousValue);
-                }
-                catch (e) {
-                    console.error(e);
-                }
-            }
-        }
-        else {
-
-            if (!__force_write_flag) {
-                return this.#stateManager.setStateValue(this.#name, newValue);
-            };
-
-            this.#value = newValue;
-
-            for (let i = 0; i < this.listeners.length; i++) {
-                let listener = this.listeners[i];
-                this.#stateManager.__callbacksToRun.push([listener, newValue, previousValue]);
-            }
-        }
-
-        return true; // is changed
-    }
-}
+import { State } from "./State";
 
 class StateManager extends EventEmitter {
     delayMs = 16
@@ -115,19 +9,19 @@ class StateManager extends EventEmitter {
     #setTimeoutId = 0
     #proxyObject = null
     __callbacksToRun = []
-    #statesIndex = 0; 
+    #statesIndex = 0;
 
     stateExists(state_name) {
         return !!this.#states[state_name];
     }
 
     createState(state_name = null, value = null) {
-        
+
         if (state_name === null) {
             state_name = `unnamed_${this.#statesIndex}`;
             this.#statesIndex++;
-        } 
-        
+        }
+
         if (this.stateExists(state_name)) {
             console.error(`State ${state_name} has been declarated already`);
             return false;
@@ -228,6 +122,7 @@ class StateManager extends EventEmitter {
         }
 
         this.__callbacksToRun = [];
+
     }
 
     waitForTasksToComplete() {
